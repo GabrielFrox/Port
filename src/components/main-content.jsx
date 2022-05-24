31750f33486d11e9efa797a823e27adae633a554
Gabriel Oliveira <gabrielfrox@gmail.com>
2022-05-24 13:23:53 -0300

    New fonts and some adjustments

--- src/components/main-content.jsx
+++ src/components/main-content.jsx
@@ -6,14 +6,14 @@ function MainContainer() {
     <div id="main-content">
       <div id='intro-container'>
       <div id="intro">
-        <h3>Olá, meu nome é</h3>
+        <h3><span>Olá, meu nome é</span></h3>
         <h1>Gabriel Oliveira dos Santos.</h1>
         <h1>Sou desenvolvedor web Front-End.</h1>
         <section id="quick-about">
           <p>
             Trilhando o caminho para o Full-Stack!
             <br/>
-            Sempre disponível para novos projetos com ideias interessantes.
+            Sempre <span>disponível</span> para novos projetos com ideias interessantes.
           </p>
         </section>
       </div>
