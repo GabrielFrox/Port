18036b815c42642b691e5f81c46bf981e3c302cd
Gabriel Oliveira <gabrielfrox@gmail.com>
2022-07-04 09:29:01 -0300

    Next commit a MVP

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <h2>Footer Here</h2>
-      <div id="animation"></div>
+      <h3>Contato</h3>
+      <p>email: gabrielfrox@gmail.com</p>
+      <p>Linkedin: <a href="https://www.linkedin.com/in/gabriel-oliveira-a8ba07159/" target='_blank' rel='noreferrer'>Gabriel Oliveira</a></p>
+      <small>Esse site se encontra em desenvolvimento</small>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,6 @@ function Header() {
     <section id="top">
       <img src={pug} alt="Shiba" id="logo"/>
       <div id="topics">
-        <h3>sobre</h3>
         <h3>contato</h3>
         <h3>links</h3>
       </div>
